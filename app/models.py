0007bb4f131275bc37938e202dbf126a23989fed
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-10-28 20:48:34 +0300

    build project structure

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    participants = models.ManyToManyField(User, related_name='rooms')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"[{self.timestamp}] {self.room.name}: {self.content}"
